3605a5f9dc83aee5c5afabb497ef849cef813250
Richard Zhang <rzhang89@jhu.edu>
2024-11-08 19:56:47 -0500

    ma

--- Assignment3/src/transport.c
+++ Assignment3/src/transport.c
@@ -230,6 +230,7 @@ static void control_loop(mysocket_t sd, context_t *ctx)
         /* see stcp_api.h or stcp_api.c for details of this function */
         /* XXX: you will need to change some of these arguments! */
         event = stcp_wait_for_event(sd, ANY_EVENT, NULL);
+        printf("another event\n");
 
         /* check whether it was the network, app, or a close request */
         if (event & APP_DATA)
@@ -245,6 +246,7 @@ static void control_loop(mysocket_t sd, context_t *ctx)
                 tcp_seq window_end = window_start + ctx->max_window_size;
 
                 while(window_end < ctx->next_seq_to_send + payload_size){//wait until there is spot in the window
+                    printf("top loop\n");
                     continue;
                 }
                 //there is window, now send
